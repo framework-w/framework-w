06c43e02546d13bbc56243f222cef784f08c4e1c
Axel Wargnier <axel@axessweb.fr>
2020-07-29 17:00:29 +0200

    add exemple controller

---
+++ app/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Controller;
+
+
+class DefaultController extends \W\Controller\Controller
+{
+
+	/**
+	 * Your homepage 
+	 */
+	public function home()
+	{
+		echo 'Hello World';
+	}
+}
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,5 @@
 <?php
 	
 $w_routes = array(
-	//['GET', '/', 'Default#home', 'default_home'],
+	['GET', '/', 'Default#home', 'default_home'],
 );
