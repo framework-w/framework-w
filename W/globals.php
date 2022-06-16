ac34621f781c43cb7c512bbed6593306187d0e01
Axel Wargnier <axel@axessweb.fr>
2022-06-16 12:14:00 +0200

    remove older functions

--- W/globals.php
+++ W/globals.php
@@ -20,31 +20,6 @@ namespace {
 		print '</pre>';
 	}
 
-	/** 
-	 *Alias de fonction 
-	 * @param $var La variable a debugger
-	 * @param $type Le type de sortie, print ou var_dump
-	 */
-	if (!function_exists('dump')){
-		function dump($var, $type = 'dump')
-		{
-			debug($var, $type);
-		}
-	}
-
-	/** 
-	 * Alias de fonction 
-	 * @param $var La variable a debugger
-	 * @param $type Le type de sortie, print ou var_dump
-	 */
-	if (!function_exists('dd')){
-		function dd($var, $type = 'dump')
-		{
-			debug($var, $type);
-			die;
-		}
-	}
-
 	/**
 	 * Retourne l'instance de l'application depuis l'espace global
 	 * @return \W\App L'application
